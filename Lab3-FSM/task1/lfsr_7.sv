090d77cf9fa93589bab3a8444dc2f5330b064a7a
Joel <nzmjoel@gmail.com>
2024-10-31 21:59:05 +0000

    added lab3

---
+++ Lab3-FSM/task1/lfsr_7.sv
@@ -0,0 +1,16 @@
+module lfsr_7 (
+    input   logic       clk,
+    input   logic       rst,
+    input   logic       en,
+    output  logic [6:0] data_out
+);
+    logic [7:1] sreg;
+
+    always_ff @ (posedge clk, posedge rst)
+        if (rst)
+            sreg <= 7'b1;
+        else if (en)
+            sreg <= {sreg[6:1], sreg[7] ^ sreg[3]};
+
+    assign data_out = sreg;
+endmodule
